e398ed97548000fe00e9e3f15a190fd5ab7ee122
jasonthorne <jasonthorne@live.ie>
2020-04-19 00:38:34 +0100

    stored procedures Out params 2

--- Workspace/4-16_StoredProcedures-OutParams/src/OutParamsDemo.java
+++ Workspace/4-16_StoredProcedures-OutParams/src/OutParamsDemo.java
@@ -1,6 +1,8 @@
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Types;
 
 public class OutParamsDemo {
 
@@ -12,13 +14,37 @@ public class OutParamsDemo {
 		String dbUrl = "jdbc:mysql://localhost:3306/demo";
 		String user = "student";
 		String pswrd = "student";
-		String department = "Engineering";
 		
 		try {
 			
+			// 1 - get connection to db:
+			connection = DriverManager.getConnection(dbUrl, user, pswrd);
 			
+			//-------------------------
 			
+			// 2 - prepare the stored procedure call:
+			callableStatement = connection.prepareCall("{call get_count_for_department(?,?)}");
 			
+			//-------------------------
+			//3 - set the parameters (used in the stored procedure):
+			
+			String department = "HR";
+			callableStatement.setString(1, department); //set the INPUT param
+			callableStatement.registerOutParameter(2, Types.INTEGER); //register the OUT param (the int declared at pos 2 of stored procedure)
+			
+			//-------------------------
+			//4 - call the stored procedure:
+			System.out.println("Calling stored procedure : get_count_for_department('" + department  + ", ?')");
+			
+			callableStatement.execute(); //execute statement +++++++++++++
+			
+			//-------------------------
+			//5 - get the value of the OUT param:
+			int statementResult = callableStatement.getInt(2);
+			
+			System.out.println("the result is: " + statementResult);
+			
+
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
