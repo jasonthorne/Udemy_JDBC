c5ddc2394d9c343984dc5d3a35a2334f2ae00d5c
jasonthorne <jasonthorne@live.ie>
2020-04-22 20:56:21 +0100

    Accessing MetaData 4

--- Workspace/5-19_AccessingMetaData/src/Examples.java
+++ Workspace/5-19_AccessingMetaData/src/Examples.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Examples {
@@ -55,7 +56,61 @@ public class Examples {
 	
 	static void SchemaInfo() throws SQLException{
 		
-		System.out.println("sup!");
+		String catalog = null;
+		String schemaPattern = null;
+		String tableNamePattern = null;
+		String coulmnNamePattern = null;
+		String[] types = null;
+		
+		Connection connection = null;
+		ResultSet resultSet = null;
+		
+		String dbUrl = "jdbc:mysql://localhost:3306/demo";
+		String user = "student";
+		String pswrd = "student";
+		
+		try{
+			
+			//--------------------------
+			//1- get a connection to the db:
+			connection = DriverManager.getConnection(dbUrl, user, pswrd);
+			
+			//-------------------------
+			//2 - get meta data: 
+			DatabaseMetaData databaseMetaData = connection.getMetaData();
+			
+			//-------------------------
+			//3 - get list of all of the tables used in schema: +++++++++++
+			
+			System.out.println("\nlist of tables:");
+			System.out.println("-----------------------");
+			
+			resultSet = databaseMetaData.getTables(catalog, schemaPattern, tableNamePattern, types);
+			
+			while(resultSet.next()) {
+				System.out.println(resultSet.getString("TABLE_NAME"));
+			}
+			
+			//-------------------------
+			//4 - get list of all of the columns used in schema: +++++++++++
+			
+			System.out.println("\nlist of columns:");
+			System.out.println("-----------------------");
+			
+			resultSet = databaseMetaData.getColumns(catalog, schemaPattern, "employees", coulmnNamePattern);
+			
+			while(resultSet.next()) {
+				System.out.println(resultSet.getString("COLUMN_NAME"));
+			}
+			
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally { //++++++++++++++++++++++++Dont forget to close these!!! 
+			connection.close(); 
+			//resultSet.close();
+		}
 		
 	}
 	
