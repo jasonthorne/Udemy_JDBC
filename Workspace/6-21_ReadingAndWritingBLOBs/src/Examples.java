e0d3524faa2b7b1d05040bb09ba84ad175070340
jasonthorne <jasonthorne@live.ie>
2020-04-25 20:12:06 +0100

    reading BLOBs from db

--- Workspace/6-21_ReadingAndWritingBLOBs/src/Examples.java
+++ Workspace/6-21_ReadingAndWritingBLOBs/src/Examples.java
@@ -1,14 +1,20 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Examples {
 	
 	
 	static void writeBLOB() throws SQLException{
+		
+		System.out.println("writing BLOB to DB:\n");
 		
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
@@ -58,9 +64,66 @@ public class Examples {
 	
 	static void readBLOB() throws SQLException{
 		
+		System.out.println("-----------------------");
+		System.out.println("reading BLOB from DB:\n");
 		
+		Connection connection = null;
+		Statement statement = null;
 		
+		String dbUrl = "jdbc:mysql://localhost:3306/demo";
+		String user = "student";
+		String pswrd = "student";
 		
+		InputStream inputStream = null;
+		FileOutputStream fileOutputStream = null;
+		
+		ResultSet resultSet = null;
+		
+		try {
+			
+			//---------------------------------------------------
+			//1 - get a connection to the db:
+			connection = DriverManager.getConnection(dbUrl, user, pswrd);
+			
+			//--------------------------------------------------
+			//2 - Execute statement:
+			
+			statement = connection.createStatement();
+			String sql = "SELECT resume FROM employees WHERE email='john.doe@foo.com'";
+			resultSet = statement.executeQuery(sql);
+			
+			//-------------------------------------------------- +++++++++++++++++
+			//3 - Set up a handle to the file
+			
+			File file = new File("PDF_from_DB.pdf"); //file obj containing path to file to be made
+			fileOutputStream = new FileOutputStream(file); //create a FileOutputStream obj, passsing in file path object
+			
+			//if result set has a result:
+			if(resultSet.next()) {
+				
+				//fill input stream with binary stream from resultSet:
+				inputStream = resultSet.getBinaryStream("resume"); 
+				
+				System.out.println("reading pdf from db");
+				System.out.println(sql);
+				
+				byte[] buffer = new byte[1024]; //create byte array to read bytes
+				
+				while(inputStream.read(buffer) > 0) { //while inputStream.read() still has bytes to read (passing in 1024 at a time) 
+					
+					fileOutputStream.write(buffer); //continue writing file, 1024 bytes at a time
+				}
+				
+				System.out.println("Saved to file: " + file.getAbsolutePath());
+				System.out.println("file created!! ");
+				
+				
+			}
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 		
 	
