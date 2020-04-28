2f45f1889a939e5d297b8fd58f502af45d16f858
jasonthorne <jasonthorne@live.ie>
2020-04-28 18:30:14 +0100

    write CLOBs 1

--- Workspace/6-22_ReadingAndWritingCLOBs/src/Examples.java
+++ Workspace/6-22_ReadingAndWritingCLOBs/src/Examples.java
@@ -1,4 +1,38 @@
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Examples {
+	
+	static void writeCLOB() throws SQLException {
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		FileReader fileReader = null;
+		
+		String dbUrl = "jdbc:mysql://localhost:3306/demo";
+		String user = "student";
+		String pswrd = "student";
+		
+		try {
+			
+			//---------------------------------------------------
+			//1- get a connection to the db:
+			connection = DriverManager.getConnection(dbUrl, user, pswrd);
+			
+			//---------------------------------------------------
+			//2 - prepare statement:
+			
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 }
--- Workspace/6-22_ReadingAndWritingCLOBs/src/Main.java
+++ Workspace/6-22_ReadingAndWritingCLOBs/src/Main.java
@@ -1,7 +1,28 @@
+import java.sql.SQLException;
 
 public class Main {
+	
+	/**
+	 * 
+	 * CLOB - Character Large Object
+	 * 
+	 * - Collection of large character data, stored as a single entity
+	 * 
+	 * - typically used to store large text documents - (plain text or XML)
+	 * 
+	 * - Note that db support for clobs is NOT UNIVERSAL!
+	 * 
+	 * - uses the LONGTEXT data type, which has support for up to 4 gigs of characters.
+	 * @throws SQLException 
+	 * 
+	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		
+		//write CLOB to DB:
+		Examples.writeCLOB();
+		
+		
 		
 
 	}
